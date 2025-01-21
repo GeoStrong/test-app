fca1d9a5bff479fc00c9a86055c6aac81b311d56
GeoStrong <dzobavageorgij@gmail.com>
2025-01-21 17:24:20 +0400

    add dark mode

--- components/header/search.tsx
+++ components/header/search.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import searchIcon from '@/public/search.svg';
+import React from "react";
+import searchIcon from "@/public/search.svg";
 
 const Search: React.FC = () => {
   return (
     <>
       <input
         type="text"
-        className={`search-input w-full border-[#e5e5e5] rounded-full border bg-[#000]/5 bg-left bg-no-repeat p-2 pl-10 text-sm font-normal text-[#737373]`}
+        className={`search-input dark:bg-dark-150 dark:border-dark-50 max-h-10 w-full rounded-full border border-[#e5e5e5] bg-[#000]/5 bg-left bg-no-repeat p-3 pl-10 text-sm font-normal text-[#737373] dark:text-white`}
         placeholder="Search ai agents..."
         style={{
           backgroundImage: `url(${searchIcon.src})`,
-          backgroundPosition: '1rem',
+          backgroundPosition: "1rem",
         }}
       />
     </>
