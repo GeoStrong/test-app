766f46545f5c495f6b0a7c46fdaf9d14e03043b5
GeoStrong <dzobavageorgij@gmail.com>
2025-01-21 23:09:44 +0400

    Refactor MainHead component for improved styling and layout consistency

--- components/main/main-head.tsx
+++ components/main/main-head.tsx
@@ -10,18 +10,18 @@ const MainHead: React.FC = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-7 md:flex-row md:items-start md:justify-between md:gap-10">
       <div className="w-full text-center md:text-left">
-        <h1 className="flex flex-col text-[40px] font-bold leading-tight text-[#0a0a0a] dark:text-white md:text-6xl">
+        <h1 className="text-dark flex flex-col text-[40px] font-bold leading-tight dark:text-white md:text-6xl">
           <span>Buy Memes.</span>
           <span>In Two Clicks.</span>
         </h1>
-        <p className="mt-2 text-sm text-[#0a0a0a] dark:text-white md:text-2xl">
+        <p className="text-dark mt-2 text-sm dark:text-white md:text-2xl">
           <span>The fastest way to buy memes in just two clicks.</span>
           <span className="block">
             Under 1 minute to sign up and no wallet needed
           </span>
         </p>
-        <div className="mt-6 hidden w-full items-center justify-between gap-4 md:flex">
-          <div className="dark:bg-dark-150 w-full rounded-xl bg-[#f5f5f5] p-4 text-[#0a0a0a] dark:text-white">
+        <div className="mt-6 hidden w-full items-center justify-between gap-4 md:flex md:flex-col lg:flex-row">
+          <div className="dark:bg-dark-150 text-dark w-full rounded-xl bg-[#f5f5f5] p-4 dark:text-white">
             <p className="flex items-center gap-2 text-sm font-normal">
               <span className="block h-2 w-2 rounded-full bg-[#22c55e]"></span>
               <span>Total users</span>
@@ -53,7 +53,7 @@ const MainHead: React.FC = () => {
               <p className="pl-10 text-2xl font-medium">52,630</p>
             </div>
           </div>
-          <div className="dark:bg-dark-150 w-full rounded-xl bg-[#f5f5f5] p-4 text-[#0a0a0a] dark:text-white">
+          <div className="dark:bg-dark-150 text-dark w-full rounded-xl bg-[#f5f5f5] p-4 dark:text-white">
             <p className="flex items-center gap-2 text-sm font-normal">
               <span className="block h-2 w-2 rounded-full bg-[#22c55e]"></span>
               <span>Trade Volume</span>
@@ -68,6 +68,7 @@ const MainHead: React.FC = () => {
         <MainCard
           price={2.74}
           marketCap="1.74B"
+          name="Virtual"
           number={0.59}
           isProfit
           isCardMain
